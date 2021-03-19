c2a66252227f363a7d9f199a4609765ef93a2a69
Deuzwood <nicolaswallet@hotmail.fr>
2021-03-19 07:29:30 +0100

    create format and vizualisation

--- script.js
+++ script.js
@@ -2,63 +2,25 @@ let stored, last;
 
 const urlParams = new URLSearchParams(window.location.search);
 
-let actualise = (name, type, time) => {
-    fetch('https://api.chess.com/pub/player/' + name + '/stats')
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            if (typeof stored === 'undefined') {
-                stored = data;
-            }
-            last = data;
-            render(type, time);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
-};
-
-let render = (game = 'bullet', time = 'all') => {
-    if (time == 'all') {
-        elo.innerHTML = stored['chess_' + game].last.rating;
-
-        win.innerHTML = stored['chess_' + game].record.win;
-        draw.innerHTML = stored['chess_' + game].record.draw;
-        loss.innerHTML = stored['chess_' + game].record.loss;
-    }
-    if (time == 'today') {
-        let modif =
-            last['chess_' + game].last.rating -
-            stored['chess_' + game].last.rating;
-        let sign = Math.sign(modif) >= 0 ? '+' : '';
-        elo.innerHTML =
-            last['chess_' + game].last.rating + ' (' + sign + modif + ')';
-
-        win.innerHTML =
-            last['chess_' + game].record.win -
-            stored['chess_' + game].record.win;
-        draw.innerHTML =
-            last['chess_' + game].record.draw -
-            stored['chess_' + game].record.draw;
-        loss.innerHTML =
-            last['chess_' + game].record.loss -
-            stored['chess_' + game].record.loss;
+let actualise = async (name, type = 'blitz', format = 'global') => {
+    try {
+        let response = await fetch(
+            'https://api.chess.com/pub/player/' + name + '/stats'
+        );
+        let data = await response.json();
+        if (typeof stored === 'undefined') {
+            stored = data;
+        }
+        console.log(data);
+        console.log(name);
+        last = data;
+        let s = getString(type, format);
+        previsualization.innerHTML = s;
+        stats.innerHTML = s;
+    } catch (err) {
+        console.error(err);
     }
 };
-console.log(urlParams.get('name'));
-
-if (urlParams.get('name') === null || urlParams.get('name') === '') {
-    container.classList = 'container';
-} else {
-    stats.classList = '';
-    actualise(
-        urlParams.get('name'),
-        urlParams.get('type'),
-        urlParams.get('time')
-    );
-    setInterval(actualise, 1000 * 60 * 0.5);
-}
 
 btn_help.addEventListener('click', (event) => {
     help.classList = help.classList.value === 'd-none' ? 'container' : 'd-none';
@@ -69,11 +31,80 @@ popout.addEventListener('click', (event) => {
     window.open(
         '/?name=' +
             document.querySelector('#name').value +
-            '&time=' +
-            time.value +
             '&type=' +
-            type.value,
+            type.value +
+            '&format=' +
+            format.value,
         '',
         'status=no, menubar=no, toolbar=no scrollbars=no'
     );
 });
+
+let getString = (type, format) => {
+    if (format == 'global') {
+        return (
+            last['chess_bullet'].last.rating +
+            ' : ' +
+            last['chess_' + type].record.win +
+            ' / ' +
+            last['chess_' + type].record.draw +
+            ' / ' +
+            last['chess_' + type].record.loss
+        );
+    } else if (format == 'session') {
+        let modif =
+            last['chess_bullet'].last.rating -
+            stored['chess_' + type].last.rating;
+        let sign = Math.sign(modif) >= 0 ? '+' : '';
+        elo = last['chess_' + type].last.rating + ' (' + sign + modif + ')';
+        win =
+            last['chess_' + type].record.win -
+            stored['chess_' + type].record.win;
+        draw =
+            last['chess_' + type].record.draw -
+            stored['chess_' + type].record.draw;
+        loss =
+            last['chess_' + type].record.loss -
+            stored['chess_' + type].record.loss;
+
+        return elo + ' : ' + win + ' / ' + draw + ' / ' + loss;
+    }
+};
+
+function updateVizualisation() {
+    actualise(
+        document.querySelector('#name').value,
+        document.querySelector('#type').value,
+        document.querySelector('#format').value
+    );
+}
+
+document.querySelector('#name').addEventListener('change', (event) => {
+    updateVizualisation();
+});
+
+document.querySelector('#type').addEventListener('change', (event) => {
+    updateVizualisation();
+});
+
+document.querySelector('#format').addEventListener('change', (event) => {
+    updateVizualisation();
+});
+
+if (urlParams.get('name') === null || urlParams.get('name') === '') {
+    container.classList = 'container';
+} else {
+    stats.classList = '';
+    actualise(
+        urlParams.get('name'),
+        urlParams.get('type'),
+        urlParams.get('format')
+    );
+    setInterval(
+        actualise,
+        1000 * 60 * 0.5,
+        urlParams.get('name'),
+        urlParams.get('type'),
+        urlParams.get('format')
+    );
+}
